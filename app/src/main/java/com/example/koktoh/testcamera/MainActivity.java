4093d77c0b1b7e488b7960668833e6f00606c1aa
koktoh <koktoh96@gmail.com>
2015-10-29 04:12:38 +0900

    カメラ実装中

--- app/src/main/java/com/example/koktoh/testcamera/MainActivity.java
+++ app/src/main/java/com/example/koktoh/testcamera/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.koktoh.testcamera;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Size;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import static android.os.Build.VERSION.SDK_INT;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CAMERA = 1;
+
+    private Size _previewSize;
+    private CameraManager _manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +35,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        _manager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            for (String id : _manager.getCameraIdList()) {
+                CameraCharacteristics info = _manager.getCameraCharacteristics(id);
+                int facing = info.get(CameraCharacteristics.LENS_FACING);
+                if (facing != CameraCharacteristics.LENS_FACING_BACK) {
+                    continue;
+                }
+                StreamConfigurationMap map = info.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                _previewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                _manager.openCamera(id, _deviceStateCallback, null);
             }
-        });
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private CameraDevice.StateCallback _deviceStateCallback = new CameraDevice.StateCallback(){
+        @Override
+    public void onOpened(CameraDevice camera){
+
+        }
+
+        @Override
+    public void onDisconnected(CameraDevice camera){
+
+        }
+
+        @Override
+    public void onError(CameraDevice camera, int error){
+
+        }
     }
 
     @Override
